d1c51a97a2cd85be5988fdea17e45ea502788023
Sandro Vieira <=>
2025-11-16 14:42:24 -0300

    feat: testing behaviors adding unit tests

---
+++ NerdStore/tests/NerdStore.Catalog.Domain.Tests/CategoryTests.cs
@@ -0,0 +1,22 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalog.Domain.Tests;
+
+public class CategoryTests
+{
+    [Theory]
+    [InlineData("", 1, "The category name cannot be empty")]
+    [InlineData("Name", 0, "The code must be greater than zero")]
+    [InlineData("Name", -1, "The code must be greater than zero")]
+    public void Category_ShouldThrowException_WhenInvalid(
+        string name,
+        int code,
+        string expectedMessage)
+    {
+        //Arrage & Act
+        var ex = Assert.Throws<DomainException>(() => new Category(name, code));
+
+        //Assert
+        Assert.Equal(expectedMessage, ex.Message);
+    }
+}
---
+++ NerdStore/tests/NerdStore.Catalog.Domain.Tests/ValueObjects/DimensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using NerdStore.Catalog.Domain.ValueObjects;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalog.Domain.Tests.ValueObjects;
+
+public class DimensionsTests
+{
+    [Theory]
+    [InlineData(0, 1, 1, "Height must be greater than zero")]
+    [InlineData(-1, 1, 1, "Height must be greater than zero")]
+    [InlineData(1, 0, 1, "Width must be greater than zero")]
+    [InlineData(1, -1, 1, "Width must be greater than zero")]
+    [InlineData(1, 1, 0, "Depth must be greater than zero")]
+    [InlineData(1, 1, -1, "Depth must be greater than zero")]
+    public void Dimensions_ShouldThrowException_WhenInvalid(
+        decimal height,
+        decimal width,
+        decimal depth,
+        string message)
+    {
+        var ex = Assert.Throws<DomainException>(() => new Dimensions(height, width, depth));
+        Assert.Equal(message, ex.Message);
+    }
+}
